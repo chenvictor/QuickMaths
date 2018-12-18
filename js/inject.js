733d86d2d954623219271bc324ba6c12e6003351
Victor Chen <chenvictor1999@gmail.com>
2018-12-17 22:59:31 -0800

    Added a settings page, and bar interface option.

--- js/inject.js
+++ js/inject.js
@@ -1,4 +1,6 @@
-if (document.getElementById("suchARandomIdwhywould131anyonepickthis124") == null) {
+const RANDOM_ID = "suchARandomIdwhywould131anyonepickthis124";
+
+if (document.getElementById(RANDOM_ID) == null) {
     let inputs = document.getElementsByTagName('input');
     for (let i = 0; i < inputs.length; i++) {
         let input = inputs[i];
@@ -6,6 +8,8 @@ if (document.getElementById("suchARandomIdwhywould131anyonepickthis124") == null
             inject(input);
         }
     }
+
+    let uiFrame;
 
 // Injects buttons onto the inputs
     function inject(input) {
@@ -27,7 +31,7 @@ if (document.getElementById("suchARandomIdwhywould131anyonepickthis124") == null
         btn.tabIndex = -1;  //don't want the user to tab to here
         btn.addEventListener("click", e => {
             e.preventDefault();
-            showFrame(input);
+            uiFrame.show(input);
         });
         input.parentNode.insertBefore(btn, input.nextSibling);
 
@@ -38,7 +42,7 @@ if (document.getElementById("suchARandomIdwhywould131anyonepickthis124") == null
             let margin = (-btnHeight - rectHeight) / 2;
             let text = "translate(" + Math.round(rect.right + document.documentElement.scrollLeft + 5) + "px , " +
                 Math.round(margin + rect.top + document.documentElement.scrollTop) +"px)";
-            btn.style.transform = text
+            btn.style.transform = text;
             // console.log("Transform to: %s", text);
             btn.style.opacity = "1";
         });
@@ -55,24 +59,44 @@ if (document.getElementById("suchARandomIdwhywould131anyonepickthis124") == null
         }
     }
 
-//add frame
+    const UI_MODAL = 0;
+    const UI_BAR = 1;
+    const FRAME_TYPE = UI_BAR;
+
+
+    let GLOBAL_INPUT;
+
+    switch (FRAME_TYPE) {
+        case UI_MODAL:
+            uiFrame = new ModalUI();
+            break;
+        case UI_BAR:
+            uiFrame = new BarUI();
+            break;
+    }
+}
+
+function ModalUI() {
+    let curInput;
+
     const frame = document.createElement("iframe");
     frame.id = "suchARandomIdwhywould131anyonepickthis124";
-    frame.style = `
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 100;
-    border: none;
-    background-color: transparent;
-    transition: opacity 0.25s ease-in-out;
-    opacity: 0;
-    display: none;
-`;
 
-    frame.src = chrome.extension.getURL("main.html");
+    frame.style = `
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 100;
+            border: none;
+            background-color: transparent;
+            transition: opacity 0.25s ease-in-out;
+            opacity: 0;
+            display: none;
+        `;
+
+    frame.src = chrome.extension.getURL("ui/modal.html");
     frame.allowTransparency = true;
     document.body.appendChild(frame);
 
@@ -83,20 +107,18 @@ if (document.getElementById("suchARandomIdwhywould131anyonepickthis124") == null
             switch (type) {
                 case 1:
                     let input = event.data.input;
-                    setInput(input);
+                    curInput.value = input;
                 case -1:                            //Fallthrough switch
-                    hideFrame();
-                    GLOBAL_INPUT.focus();
+                    hide();
+                    curInput.focus();
                     return;
             }
         }
         console.warn("Received message: %o", event.data);
     });
 
-    let GLOBAL_INPUT;
-
-    function showFrame(input) {
-        GLOBAL_INPUT = input;
+    function show(input) {
+        curInput = input;
 
         frame.contentWindow.postMessage({
             type: 0,
@@ -109,16 +131,83 @@ if (document.getElementById("suchARandomIdwhywould131anyonepickthis124") == null
         }, 5);  //small delay to allow CSS to transition properly
     }
 
-    function setInput(value) {
-        GLOBAL_INPUT.value = value;
-    }
-
-    function hideFrame() {
+    function hide() {
         frame.style.opacity = "0";
         setTimeout(() => {
             frame.style.display = "none";
         }, 250);
     }
+    return {
+        show: show,
+        hide: hide
+    }
+}
 
+function BarUI() {
+    let curInput;
 
+    const frame = document.createElement("iframe");
+    frame.id = "suchARandomIdwhywould131anyonepickthis124";
+
+    frame.scrolling = "no";
+    frame.style = `
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            height: 100%;
+            width: 100%;
+            z-index: 100;
+            border: none;
+            transition: opacity 0.25s ease-in-out;
+            opacity: 0;
+            background: none;
+            display: none;
+        `;
+
+    frame.src = chrome.extension.getURL("ui/bar.html");
+    frame.allowTransparency = true;
+    document.body.appendChild(frame);
+
+    addEventListener("message", function (event) {
+        if (event.data.hasOwnProperty("type")) {
+            let type = event.data.type;
+            // noinspection FallThroughInSwitchStatementJS
+            switch (type) {
+                case 1:
+                    let input = event.data.input;
+                    curInput.value = input;
+                case -1:                            //Fallthrough switch
+                    hide();
+                    curInput.focus();
+                    return;
+            }
+        }
+        console.warn("Received message: %o", event.data);
+    });
+
+    function show(input) {
+        curInput = input;
+
+        frame.allowTransparency = true;
+        frame.contentWindow.postMessage({
+            type: 0,
+            input: input.value,
+        }, "*");
+
+        frame.style.display = "inline";
+        setTimeout(() => {
+            frame.style.opacity = "1";
+        }, 5);  //small delay to allow CSS to transition properly
+    }
+
+    function hide() {
+        frame.style.opacity = "0";
+        setTimeout(() => {
+            frame.style.display = "none";
+        }, 250);
+    }
+    return {
+        show: show,
+        hide: hide
+    }
 }
